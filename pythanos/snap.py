26a142790af7f96a43f789d6e7311c140a25bec0
TanweerulHaque <tan.haque@gmail.com>
2021-09-21 21:54:01 +0530

    added split,fixed,ratio,progbar etc. Still bugs are there.

--- pythanos/snap.py
+++ pythanos/snap.py
@@ -6,6 +6,7 @@ import re
 import urllib.request
 import concurrent.futures
 import time
+from os import path
 
 
 class Snap:
@@ -18,6 +19,14 @@ class Snap:
             Path.cwd(), self.download_file_name)
         self.download_save_path = self.download_complete_path.parent
         self.run_loader = False
+        self.split_ratio = (0.8, 0.1, 0.1)
+        self.split_fixed = (100, 100)
+        self.seed = 69
+        self.output = "output"
+        self.split_train_idx = None
+        self.split_val_idx = None
+        self.use_test = False
+        self.create_mode = f'copy'
 
     def input_type(self) -> None:
         try:
@@ -119,8 +128,120 @@ class Snap:
             i += 1
             if i >= 4:
                 i = 0
-        print("\r", f"{message} : ", f"DONE", end=f'', flush=True)
+        print("\r", f"{message} : ", f"DONE", end=f'  ', flush=True)
 
+    def ratio(self, output="output", seed=69,
+              ratio=(0.8, 0.1, 0.1), create_mode='copy'):
+
+        assert round(sum(ratio), 10) == 1, f"Sum of Ratio must be 1."
+        assert len(ratio) in (2, 3), f"Invalid split ratio tuple dimension"
+        assert create_mode in ('copy', 'move'), f"Invalid create mode"
+
+        self.split_ratio = ratio
+        self.seed = seed
+        self.output = output
+        self.create_mode = create_mode
+
+        for class_dir in self.list_dirs(self.inputvar):
+            self.split_class_dir_ratio(class_dir)
+
+    def fixed(self, output="output", seed=69,
+              fixed=(100, 100), create_mode='copy'):
+
+        assert len(fixed) in (1, 2), f"Invalid fixed split tuple dimension"
+        assert create_mode in ('copy', 'move'), f"Invalid create mode"
+
+        self.split_fixed = fixed
+        self.seed = seed
+        self.output = output
+        self.create_mode = create_mode
+
+        dirs = self.list_dirs(self.inputvar)
+        lens = []
+        for class_dir in dirs:
+            lens.append(
+                self.split_class_dir_fixed(
+                    class_dir,
+                    self.output,
+                    self.split_fixed,
+                    self.seed,
+                )
+            )
+
+    def setup_files(self, class_dir):
+        random.seed(self.seed)
+        files = self.list_files(class_dir)
+        random.shuffle(files)
+        return files
+
+    def split_class_dir_ratio(self, class_dir):
+        files = self.setup_files(class_dir, self.seed)
+
+        self.split_train_idx = int(self.ratio[0] * len(files))
+        self.split_val_idx = self.split_train_idx + \
+            int(self.ratio[1] * len(files))
+        self.use_test = len(self.ratio) == 3
+
+        li = self.split_files(files)
+        if self.create_mode == 'copy':
+            self.copy_files(li, class_dir, self.output)
+        else:
+            self.move_files(li, class_dir, self.output)
+
+    def split_class_dir_fixed(self, class_dir):
+        files = self.setup_files(class_dir, self.seed)
+
+        if not len(files) > sum(self.split_fixed):
+            raise ValueError()
+
+        self.split_train_idx = len(files) - sum(self.split_fixed)
+        self.split_val_idx = self.split_train_idx + self.split_fixed[0]
+        self.use_test = len(self.split_fixed) == 2
+
+        li = self.split_files(files)
+        if self.create_mode == 'copy':
+            self.copy_files(li, class_dir, self.output)
+        else:
+            self.move_files(li, class_dir, self.output)
+        return len(files)
+
+    def split_files(self, files):
+        files_train = files[:self.split_train_idx]
+        files_val = (files[self.split_train_idx:self.split_val_idx]
+                     if self.use_test else files[self.split_train_idx:])
+
+        li = [(files_train, "train"), (files_val, "val")]
+
+        if self.use_test:
+            files_test = files[self.split_val_idx:]
+            li.append((files_test, "test"))
+        return li
+
+    def copy_files(self, files_type, class_dir):
+        class_name = path.split(class_dir)[1]
+        for (files, folder_type) in files_type:
+            full_path = path.join(self.output, folder_type, class_name)
+
+            Path(full_path).mkdir(parents=True, exist_ok=True)
+            for f in files:
+                if type(f) == tuple:
+                    for x in f:
+                        shutil.copy2(x, full_path)
+                else:
+                    shutil.copy2(f, full_path)
+
+    def move_files(self, files_type, class_dir):
+        class_name = path.split(class_dir)[1]
+        for (files, folder_type) in files_type:
+            full_path = path.join(self.output, folder_type, class_name)
+
+            Path(full_path).mkdir(parents=True, exist_ok=True)
+            for f in files:
+                if type(f) == tuple:
+                    for x in f:
+                        shutil.move(x, full_path)
+                else:
+                    shutil.move(f, full_path)
 
 # abcd = r"""http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg"""
 # Snap(abcd).download_from_url(
@@ -132,4 +253,4 @@ class Snap:
 #     x.progbar(i, 100066)
 # print()
 
-Snap("").zip()
+# Snap("").zip()
